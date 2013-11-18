4f8ce3b56cd6f6612cc3a9a06fcbd1244610d435
Viktor Lazarev <taurus101v@gmail.com>
2013-11-18 22:22:30 +0400

    project.clj - dependencies and description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject cblog "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Simple collective blog with authorisation"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [lib-noir "0.7.5"]
@@ -9,7 +9,10 @@
                  [com.taoensso/timbre "2.7.1"]
                  [com.postspectacular/rotor "0.1.0"]
                  [com.taoensso/tower "1.7.1"]
-                 [markdown-clj "0.9.35"]]
+                 [markdown-clj "0.9.35"]
+                 [com.cemerick/friend "0.2.0"]
+                 [korma "0.3.0-RC6"]
+                 [mysql/mysql-connector-java "5.1.6"]]
   :aot :all
   :plugins [[lein-ring "0.8.7"]]
   :ring {:handler cblog.handler/app
