c68902737d8fb238b712d07f1ed6a5e80c09a08c
Roman <felixcon@gmail.com>
2013-11-19 00:34:00 +0400

    initial commit

---
+++ src/cblog/views/layout.clj
@@ -0,0 +1,32 @@
+(ns cblog.views.layout
+  (:require [selmer.parser :as parser]
+            [clojure.string :as s]
+            [ring.util.response :refer [content-type response]]
+            [noir.session :as session])
+  (:import compojure.response.Renderable))
+
+(def template-path "cblog/views/templates/")
+
+(deftype
+  RenderableTemplate
+  [template params]
+  Renderable
+  (render
+    [this request]
+    (content-type
+      (->>
+        (assoc
+          params
+          (keyword (s/replace template #".html" "-selected"))
+          "active"
+          :servlet-context
+          (:context request)
+          :user-id
+          (session/get :user-id))
+        (parser/render-file (str template-path template))
+        response)
+      "text/html; charset=utf-8")))
+
+(defn render [template & [params]]
+  (RenderableTemplate. template params))
+
