56de5c37f8da423f4cf022957b22fe888684c166
Viktor Lazarev <taurus101v@gmail.com>
2013-11-19 14:59:29 +0400

    init DB

--- src/cblog/models/schema.clj
+++ src/cblog/models/schema.clj
@@ -4,24 +4,24 @@
 (def db-spec
   {:subprotocol "postgresql"
    :subname "//localhost/cblog"
-   :user "admin"
-   :password "admin"})
+   :user "cblog"
+   :password "cblog"})
 
-(defn initialized? []
-  (throw (new Exception "TODO: initialize the database schema!")))
+(defn initialized? [] true)
 
 (defn create-users-table []
   (sql/with-connection db-spec
     (sql/create-table
       :users
       [:id "varchar(20) PRIMARY KEY"]
-      [:first_name "varchar(30)"]
-      [:last_name "varchar(30)"]
-      [:email "varchar(30)"]
+      [:first_name "varchar(35)"]
+      [:last_name "varchar(35)"]
+      [:email "varchar(320)"]  ; in accordance with RFC 3696
       [:admin :boolean]
       [:last_login :time]
       [:is_active :boolean]
-      [:pass "varchar(100)"])))
+      [:pass "varchar(100)"]
+      [:salt "varchar(20)"])))
 
 (defn create-tables
   "creates the database tables used by the application"
