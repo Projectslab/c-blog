abe2f278da47332764a6e7aafdc7da2642c664db
Roman <felixcon@gmail.com>
2013-11-23 23:21:40 +0400

    v0.13

--- src/cblog/config/schema.clj
+++ src/cblog/config/schema.clj
@@ -3,19 +3,19 @@
             [cblog.config.db :refer [db-spec]]))
 
 
-(defn create-users-table []
-  (sql/with-connection db-spec
-    (sql/create-table
-      :users
-      [:id "SERIAL"]
-      [:name "varchar(30)"]
-      [:email "varchar(30)"]
-      [:admin :boolean]
-      [:last_login :time]
-      [:is_active :boolean]
-      [:pass "varchar(100)"])))
+;(defn create-users-table []
+;  (sql/with-connection db-spec
+;    (sql/create-table
+;      :users
+;      [:id "SERIAL"]
+;      [:name "varchar(30)"]
+;      [:email "varchar(30)"]
+;      [:admin :boolean]
+;      [:last_login :time]
+;      [:is_active :boolean]
+;      [:pass "varchar(100)"])))
 
-(defn create-tables
-  "creates the database tables used by the application"
-  []
-  (create-users-table))
+;(defn create-tables
+;  "creates the database tables used by the application"
+;  []
+;  (create-users-table))
