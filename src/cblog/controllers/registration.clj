d8f7787fd1a7f1d251f0cbc6023c22c7b797fef9
Roman <felixcon@gmail.com>
2013-11-23 17:37:46 +0400

    v0.11

--- src/cblog/controllers/registration.clj
+++ src/cblog/controllers/registration.clj
@@ -28,21 +28,22 @@
     {
      :myname myname
      :email email
+     :any-error (vali/on-error :any-error first)
      :myname-error (vali/on-error :myname first)
      :email-error (vali/on-error :email first)
      :pass-error (vali/on-error :pass first)
      :pass1-error (vali/on-error :pass1 first)}))
-(register "Roman" "mail@mail.com")
+
 
 (defn handle-registration [myname email pass pass1]
   (if (valid-registration? myname email pass pass1)
     (try
       (do
-        (db-user/create-user {:myname myname :email email :pass (crypt/encrypt pass)})
+        (db-user/create-user {:name myname :email email :pass (crypt/encrypt pass)})
         (session/put! :user-id email)
         (resp/redirect "/"))
       (catch Exception ex
-        (vali/rule false [:id (.getMessage ex)])
+        (vali/rule false [:any-error (.getMessage ex)])
         (register)))
     (register myname email)))
 
