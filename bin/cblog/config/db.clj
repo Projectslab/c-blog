7d6f86ed1e420646d042099d52fca69de2a7a225
Roman <felixcon@gmail.com>
2013-11-25 06:43:48 +0400

    v0.32

--- bin/cblog/config/db.clj
+++
@@ -1,8 +0,0 @@
-(ns cblog.config.db
-  (:require [clojure.java.jdbc :as sql]))
-
-(def db-spec
-  {:subprotocol "postgresql"
-   :subname "//localhost/blog"
-   :user "postgres"
-   :password "gfhjkm"})
