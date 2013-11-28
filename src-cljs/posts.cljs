f2370411c269d3fe5139e88ab0b3a4f88aa785d8
Viktor Lazarev <taurus101v@gmail.com>
2013-11-28 13:18:34 +0400

    Show / hide new post form

--- src-cljs/posts.cljs
+++ src-cljs/posts.cljs
@@ -1,10 +1,11 @@
 (ns cblog.posts
   (:require [ajax.core :refer [GET POST PUT]]
-            [domina :refer [by-id]]
-            [domina.events :refer [listen! prevent-default stop-propagation]]))
+            [domina :refer [by-id styles set-styles!]]
+            [domina.events :refer [listen! prevent-default stop-propagation]]
+            [domina.css :refer [sel]]))
 
 (defn validate_form []
-  (listen! (by-id "new-post") :submit
+  (listen! (sel "#new-post-wrapper > form") :submit
            (fn [e]
              (do
                (.log js/console "blah-blah")
@@ -12,5 +13,16 @@
                (stop-propagation e)
                false))))
 
+(defn trigger-new-post-form []
+  (let [new-post (sel "#new-post-wrapper")]
+    (listen! (by-id "new-post-button") :click
+             (fn [e]
+               (set-styles! new-post
+                            {:display (if (= (:display (styles new-post)) "none")
+                                        "block"
+                                        "none")})))))
+
+
 (defn ^:export init []
-  (validate_form))
+  (validate_form)
+  (trigger-new-post-form))
