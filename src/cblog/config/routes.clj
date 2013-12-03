fd501c871ff6f39e2914a9b41cfebe3f817b6fcf
Viktor Lazarev <taurus101v@gmail.com>
2013-12-03 07:52:45 +0400

    Remove cljsexample

--- src/cblog/config/routes.clj
+++ src/cblog/config/routes.clj
@@ -58,22 +58,6 @@
   (POST "/session/" []
         (session/destroy)))
 
-;;;;;;;;;;;;;;;; CLJS routes ;;;;;;;;;;;;;;;;;;;;;;;
-
-(def messages
-  (atom
-    [{:message "Hello world"
-      :user    "Foo"}
-     {:message "Ajax is fun"
-      :user    "Bar"}]))
-
-(defroutes cljs-routes
-  (GET "/cljsexample" [] (layout/render "cljsexample.html"))
-  (GET "/messages" [] (response/edn @messages))
-  (POST "/add-message" [message user]
-        (response/edn
-          (swap! messages conj {:message message :user user}))))
-
 ;;;;;;;;;;;;;;;;;  Home routes ;;;;;;;;;;;;;;;;;;;
 
 ;; List all posts on index page
